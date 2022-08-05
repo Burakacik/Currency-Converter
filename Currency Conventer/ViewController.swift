b36e4656b2ae9b508328c67cf6b03fa4ed05ceff
Burak AÇIK <burakacik@outlook.com>
2022-08-05 18:55:04 +0300

    Storyboard Designed

--- Currency Conventer/ViewController.swift
+++ Currency Conventer/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        
 
+    }
+    @IBAction func convertButtonClicked(_ sender: Any) {
+    }
+    
 
 }
 
